4107cd83b146d07495e670bd5fffa58db801f500
이윤주 <toyaann@naver.com>
2022-02-17 15:13:13 +0900

    RoomAdapter 클래스 생성   - ArrayAdapter 상속   - 생성자 수정 > 부모클래스에게 넘겨줄 자료 받아오기

--- app/src/main/java/com/nepplus/jickbangcopy_20220217/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220217/adapters/RoomAdapter.kt
@@ -8,16 +8,12 @@ import android.widget.ArrayAdapter
 import com.nepplus.jickbangcopy_20220217.datas.RoomData
 
 class RoomAdapter(
-    private val mContext: Context,
+    val mContext: Context,
     val resId: Int,
     val mList: ArrayList<RoomData>
-):ArrayAdapter<RoomData>(mContext,resId,mList) {
+) : ArrayAdapter<RoomData>(mContext, resId, mList){
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var tempRow == convertView
-        if(tempRow==null){
-            tempRow = LayoutInflater.from(mContext).inflate(R.layout.)
-        }
+
+
     }
 
-}
